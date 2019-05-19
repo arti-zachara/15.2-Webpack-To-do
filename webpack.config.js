1768c9b18671b458c4e673f0be09f796296f70ae
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-05-19 18:19:56 +0200

    Change dev dependencies to babel-loader@7 to match the proper babel-core

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "index.boundle.js"
+    filename: "app.boundle.js"
   },
   module: {
     rules: [
